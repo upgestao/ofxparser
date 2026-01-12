562bdfc5f48e23e74938de82522dcbcd8fd2eea7
Guilherme Melaninho <guilherme.melaninho@gmail.com>
2026-01-12 09:11:49 -0300

    ajustando encode_arquivo

--- lib/OfxParser/Parser.php
+++ lib/OfxParser/Parser.php
@@ -50,7 +50,7 @@ class Parser
     public function loadFromString($ofxContent)
     {
         $ofxContent = str_replace(["\r\n", "\r"], "\n", $ofxContent);
-        $ofxContent = utf8_encode($ofxContent);
+        $ofxContent = mb_convert_encoding($ofxContent, 'UTF-8', 'ISO-8859-1');
 
         $sgmlStart = stripos($ofxContent, '<OFX>');
         $ofxHeader =  trim(substr($ofxContent, 0, $sgmlStart));
